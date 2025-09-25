66670766f53c6c73ea810076ccbf64f35aaa095d
Rafael Hickmann <rafahck@gmail.com>
2025-09-25 14:54:35 -0300

    arrumando tudo

--- src/components/speech/index.tsx
+++ src/components/speech/index.tsx
@@ -155,7 +155,7 @@ export const SystemAudio = ({
           className="select-none w-screen p-0 border overflow-hidden border-input/50"
           sideOffset={18}
         >
-          <ScrollArea className="h-[600px] overflow-y-auto">
+          <ScrollArea className="max-h-[600px] overflow-y-auto">
             <div
               className={`p-6 ${
                 !lastTranscription && !lastAIResponse
